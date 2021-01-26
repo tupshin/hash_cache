957e05308a79ba8d988e0d773b1a6a4eed92a9c7
Tupshin Harper <tupshin@tupshin.com>
2021-01-25 23:18:33 -0500

    implemented most tests and benchmarking

--- src/hash_cache.rs
+++ src/hash_cache.rs
@@ -3,34 +3,46 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::time::Duration;
 use std::time::SystemTime;
+use std::fmt::Debug;
+use std::sync::{Mutex, Arc};
 
-#[derive(Default,Debug)]
+#[derive(Default,Debug,Clone)]
 pub struct HashCache<K, V> {
-    cache: HashMap<K, (Option<SystemTime>, V)>,
+    pub cache: Arc<Mutex<HashMap<K, (Option<SystemTime>, V)>>>,
 }
 
-impl<K: Clone + Eq + Hash, V: Clone> KVStore<K, V> for HashCache<K, V> {
+unsafe impl<K,V> Send for HashCache<K,V> {}
+unsafe impl<K,V> Sync for HashCache<K,V> {}
+
+impl<K: Clone + Eq + Hash + Debug, V: Clone + Debug> KVStore<K, V> for HashCache<K, V> {
     fn put(&mut self, k: K, v: V, expiration: Option<Duration>) -> Result<(), KVError> {
         match expiration {
-            Some(exp) => self.cache.insert(k, (Some(SystemTime::now() + exp), v)),
-            None => self.cache.insert(k, (None, v)),
+            Some(exp) => self.cache.lock().unwrap().insert(k, (Some(SystemTime::now() + exp), v)),
+            None => self.cache.lock().unwrap().insert(k, (None, v)),
         };
         Ok(())
     }
 
     fn get(&mut self, k: K) -> Option<V> {
-        match self.cache.get(&k) {
-            None => None,
+        let mut cache = self.cache.lock().unwrap();
+        match cache.get(&k) {
+            None => {
+                //No value found
+                None
+            }
             Some((t, v)) => {
                 match t {
-                    None => None,
+                    None => {
+                        //No expiration time, so key always valid
+                        Some(v.clone())
+                    },
                     Some(t) => {
                         if t > &SystemTime::now() {
                             //key still valid
                             Some(v.clone())
                         } else {
                             //key expired
-                            self.cache.remove(&k);
+                            cache.remove(&k);
                             None
                         }
                     }
@@ -40,7 +52,7 @@ impl<K: Clone + Eq + Hash, V: Clone> KVStore<K, V> for HashCache<K, V> {
     }
 
     fn delete(&mut self, k: K) -> Result<(), KVError> {
-        self.cache.remove(&k);
+        self.cache.lock().unwrap().remove(&k);
         Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod hash_cache;
 mod tests {
     use crate::api::KVStore;
     use crate::hash_cache::HashCache;
+    use std::thread;
     use std::time::Duration;
 
     #[test]
@@ -21,7 +22,6 @@ mod tests {
     fn test_get() {
         let mut cache = HashCache::default();
         cache.put("a", "b", None).unwrap();
-        dbg!(&cache);
         let result = cache.get("a").unwrap();
         assert_eq!(result, "b");
     }
@@ -39,8 +39,76 @@ mod tests {
 
     #[test]
     fn test_expiring_put() {
-        let mut cache = HashCache::default();
-        cache.put("a", "b", Some(Duration::new(1, 0))).unwrap();
+        let mut cache1 = HashCache::default();
+        //This clones the internal Arc
+        //let mut cache2 = cache1.clone();
+        let duration = Duration::new(1, 0);
+        cache1.put("a", "b", Some(duration)).unwrap();
 
+        assert_eq!(Some("b"), cache1.get("a"));
+        thread::sleep(duration);
+        assert_eq!(None, cache1.get("a"));
+    }
+
+    #[test]
+    fn multi_threaded_put_get() {
+        let cache = HashCache::default().cache;
+        let cache1 = cache.clone();
+        let cache2 = cache1.clone();
+
+        let thread_one = thread::spawn(move || {
+            cache1.lock().unwrap().insert("A", (None, "a"));
+        });
+        let thread_two = thread::spawn(move || {
+            cache2.lock().unwrap().insert("B", (None, "b"));
+        });
+
+        thread_one.join().unwrap();
+        thread_two.join().unwrap();
+
+        thread::spawn(move || {
+            assert_eq!(cache.lock().unwrap().get("A").unwrap().1, "a");
+            assert_eq!(cache.lock().unwrap().get("B").unwrap().1, "b");
+        });
+    }
+
+    #[test]
+    fn bulk_put_get() {
+        let cache = HashCache::default().cache;
+        let cache1 = cache.clone();
+        let cache2 = cache1.clone();
+
+        let thread_one = thread::spawn(move || {
+            for i in 1..=1000 {
+                cache1.lock().unwrap().insert(i, (None, format!("v:{}", i)));
+            }
+        });
+        thread_one.join().unwrap();
+
+        let thread_two = thread::spawn(move || {
+            assert_eq!(cache2.lock().unwrap().get(&75).unwrap().1, format!("v:{}", 75));
+            assert_eq!(cache2.lock().unwrap().get(&352).unwrap().1, format!("v:{}", 352));
+        });
+        thread_two.join().unwrap();
+    }
+
+    #[test]
+    fn ten_million_load() {
+        let cache = HashCache::default().cache;
+        let cache1 = cache.clone();
+        let cache2 = cache1.clone();
+
+        let thread_one = thread::spawn(move || {
+            for i in 1..=1_000_000 {
+                cache1.lock().unwrap().insert(i, (None, format!("v:{}", i)));
+            }
+        });
+        thread_one.join().unwrap();
+
+        let thread_two = thread::spawn(move || {
+            assert_eq!(cache2.lock().unwrap().get(&75).unwrap().1, format!("v:{}", 75));
+            assert_eq!(cache2.lock().unwrap().get(&352).unwrap().1, format!("v:{}", 352));
+        });
+        thread_two.join().unwrap();
     }
 }
